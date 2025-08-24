c18dac663666b8e4ce6649818fc4f5cd7839c817
Farzam <f_yamini72@yahoo.com>
2025-08-24 11:36:23 +0330

    UserList component with filtering

--- SolutionExplorer.KMS.SharedUI/Dtos/UserDtos/UserDisplayDto.cs
+++ SolutionExplorer.KMS.SharedUI/Dtos/UserDtos/UserDisplayDto.cs
@@ -1,15 +1,25 @@
-﻿namespace SolutionExplorer.KMS.SharedUI.Dtos.UserDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SolutionExplorer.KMS.SharedUI.Dtos.UserDtos
 {
     public class UserDisplayDto : BaseDto
     {
+        [Display(Name = "نام کاربری")]
         public string UserName { get; set; }
+
+        [Display(Name = "نام")]
         public string FirstName { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
         public string LastName { get; set; }
 
+        [Display(Name = "نقش‌ها")]
         public List<UserRoleDisplayDto> UserRoles { get; set; }
 
-
+        [Display(Name = "شناسه تصویر پروفایل")]
         public int? ProfileId { get; set; }
+
+        [Display(Name = "آدرس تصویر پروفایل")]
         public string? AttachmentUrl { get; set; }
     }
 }
